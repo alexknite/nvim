1f461ad035db2ec6bd9d09db7b0cf0943a3de915
alexknite <alexniteo@gmail.com>
2025-03-20 13:54:56 -0700

    Autocompletion, formatting, & linting

--- lua/config/plugins/nvimtree.lua
+++ lua/config/plugins/nvimtree.lua
@@ -1,9 +1,9 @@
 return {
     "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("nvim-tree").setup()
-        vim.keymap.set("n", "<leader>r", "<CMD>NvimTreeFocus<CR>")
-        vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeToggle<CR>")
-    end
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("nvim-tree").setup()
+    vim.keymap.set("n", "<leader>r", "<CMD>NvimTreeFocus<CR>")
+    vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeToggle<CR>")
+  end
 }
