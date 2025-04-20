fca922094a6b1bdd74479f5a35303e625f9834a8
Alex Nite <me@alexnite.com>
2025-04-19 19:51:53 -0700

    Update code-runner.lua

--- lua/config/plugins/code-runner.lua
+++ lua/config/plugins/code-runner.lua
@@ -1,40 +1,48 @@
 return {
--- 	"hkupty/iron.nvim",
--- 	config = function()
--- 		local iron = require("iron.core")
+  "hkupty/iron.nvim",
+  config = function()
+    local iron = require("iron.core")
 
--- 		iron.setup({
--- 			config = {
--- 				scratch_repl = true,
--- 				repl_definition = {
--- 					python = {
--- 						command = { "python3" },
--- 					},
--- 				},
--- 				repl_open_cmd = require("iron.view").right(60),
--- 			},
--- 			keymaps = {
--- 				send_motion = "<space>rc",
--- 				visual_send = "<space>rc",
--- 				send_file = "<space>rf",
--- 				send_line = "<space>rl",
--- 				send_mark = "<space>rm",
--- 				mark_motion = "<space>rmc",
--- 				mark_visual = "<space>rmc",
--- 				remove_mark = "<space>rmd",
--- 				cr = "<space>r<cr>",
--- 				interrupt = "<space>r<space>",
--- 				exit = "<space>rq",
--- 				clear = "<space>rx",
--- 			},
--- 			highlight = {
--- 				italic = true,
--- 			},
--- 			ignore_blank_lines = true,
--- 		})
--- 		vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
--- 		vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
--- 		vim.keymap.set("n", "<space>rF", "<cmd>IronFocus<cr>")
--- 		vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
--- 	end,
+    iron.setup({
+      config = {
+        scratch_repl = true,
+        repl_definition = {
+          python = {
+            command = { "python3" },
+          },
+        },
+        repl_open_cmd = require("iron.view").right(80),
+      },
+      keymaps = {
+        toggle_repl = "<space>rr", -- toggles the repl open and closed.
+        -- If repl_open_command is a table as above, then the following keymaps are
+        -- available
+        -- toggle_repl_with_cmd_1 = "<space>rv",
+        -- toggle_repl_with_cmd_2 = "<space>rh",
+        restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
+        send_motion = "<space>sc",
+        visual_send = "<space>sc",
+        send_file = "<space>sf",
+        send_line = "<space>sl",
+        send_paragraph = "<space>sp",
+        send_until_cursor = "<space>su",
+        send_mark = "<space>sm",
+        send_code_block = "<space>sb",
+        send_code_block_and_move = "<space>sn",
+        mark_motion = "<space>mc",
+        mark_visual = "<space>mc",
+        remove_mark = "<space>md",
+        cr = "<space>s<cr>",
+        interrupt = "<space>s<space>",
+        exit = "<space>sq",
+        clear = "<space>cl",
+      },
+      highlight = {
+        italic = true,
+      },
+      ignore_blank_lines = true,
+    })
+    vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+    vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+  end,
 }
