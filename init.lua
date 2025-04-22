07c6238c48e517dc063aad949eecb30ad2aab227
Alex Nite <me@alexnite.com>
2025-04-21 22:05:39 -0700

    Fix transparency on Windows

--- init.lua
+++ init.lua
@@ -2,3 +2,32 @@ require("config.settings")
 require("config.keymaps")
 require("config.lazy")
 
+-- Function to apply transparency settings globally (incl. Nvim-Tree)
+local function set_transparency()
+	vim.cmd([[
+    hi Normal guibg=NONE ctermbg=NONE
+    hi NormalNC guibg=NONE ctermbg=NONE
+    hi SignColumn guibg=NONE ctermbg=NONE
+    hi StatusLine guibg=NONE ctermbg=NONE
+    hi StatusLineNC guibg=NONE ctermbg=NONE
+    hi VertSplit guibg=NONE ctermbg=NONE
+    hi TabLine guibg=NONE ctermbg=NONE
+    hi TabLineFill guibg=NONE ctermbg=NONE
+    hi TabLineSel guibg=NONE ctermbg=NONE
+    hi Pmenu guibg=NONE ctermbg=NONE
+    hi PmenuSel guibg=NONE ctermbg=NONE
+    hi EndOfBuffer guibg=NONE ctermbg=NONE
+    hi NvimTreeNormal guibg=NONE ctermbg=NONE
+    hi NvimTreeNormalNC guibg=NONE ctermbg=NONE
+    hi NvimTreeWinSeparator guibg=NONE ctermbg=NONE
+  ]])
+end
+
+-- Apply transparency settings initially
+set_transparency()
+
+-- Reapply on BufEnter and ColorScheme change
+vim.api.nvim_create_autocmd({ "BufEnter", "ColorScheme" }, {
+	pattern = "*",
+	callback = set_transparency,
+})
