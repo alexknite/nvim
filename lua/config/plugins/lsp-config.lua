9aa053c14e0912412e6b23a60cb6c47e66a48332
Alex Nite <me@alexnite.com>
2026-01-01 16:49:35 -0800

    Update plugins

--- lua/config/plugins/lsp-config.lua
+++ lua/config/plugins/lsp-config.lua
@@ -5,48 +5,56 @@ return {
       require("mason").setup()
     end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd", "html", "cssls", "ts_ls", "ast_grep", "ruby_lsp" },
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "clangd",
+          "html",
+          "cssls",
+          "ts_ls",
+          "ast_grep",
+          "ruby_lsp",
+        },
       })
     end,
   },
+
   {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
+      -- Define shared defaults
+      local default_config = {
         capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ast_grep.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ruby_lsp.setup({
-        capabilities = capabilities,
-      })
+      }
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      -- Configure servers
+      local servers = {
+        "lua_ls",
+        "pyright",
+        "clangd",
+        "html",
+        "cssls",
+        "ts_ls",
+        "ast_grep",
+        "ruby_lsp",
+      }
+
+      for _, server in ipairs(servers) do
+        vim.lsp.config(server, default_config)
+        vim.lsp.enable(server)
+      end
+
+      -- Keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
     end,
   },
 }
