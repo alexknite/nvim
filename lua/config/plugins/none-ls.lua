6f0bfff482d9e5f483f39f12a5da4cedcf36e5af
Alex Nite <me@alexnite.com>
2026-01-02 21:55:27 -0800

    Fix none-ls for python

--- lua/config/plugins/none-ls.lua
+++ lua/config/plugins/none-ls.lua
@@ -1,21 +1,32 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint_d"),
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.rubocop,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				-- Lua
+				null_ls.builtins.formatting.stylua,
+
+				-- Python
+				null_ls.builtins.formatting.black,
+				require("none-ls.formatting.ruff"),
+
+				-- JS / TS
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d"),
+
+				-- Ruby
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.diagnostics.rubocop,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", function()
+			vim.lsp.buf.format({ async = false })
+		end)
+	end,
 }
